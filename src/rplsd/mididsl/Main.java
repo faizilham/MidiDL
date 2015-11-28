5940c119441cbfdece53bb98bf51b512aa64e4b4
Faiz Ilham <indigo.republica@gmail.com>
2015-11-28 12:56:23 +0700

    set main for inline command compiler

--- src/rplsd/mididsl/Main.java
+++ src/rplsd/mididsl/Main.java
@@ -9,12 +9,30 @@ import rplsd.mididsl.model.MidiObject;
 
 public class Main {
 	public static void main(String[] args) throws Exception{
+		String outputfile = null;
+		if (args.length < 1) {
+			System.out.println("Usage example: mididl input.mdl [output.midi].");
+			System.out.println("If no output file, it will play the generated midi.");
+		}
+		
+		String inputfile = args[0];
+		if (args.length > 1){
+			outputfile = args[1];
+		}
+				
 		try {
 			MdlReader reader = new MdlReader();
-			MidiObject midi = reader.buildModel(new FileInputStream(new File("test.mdl")));
-			//System.out.println(midi);
+			System.out.println("Generate midi");
+			MidiObject midi = reader.buildModel(new FileInputStream(new File(inputfile)));
+			MidiGenerator gen = new MidiGenerator(midi);
 			
-			MidiGenerator gen = new MidiGenerator(midi); gen.play(); //gen.write(new File("test.midi"));
+			if (outputfile == null){
+				System.out.println("Playing midi");
+				gen.play();
+			} else {
+				System.out.println("Output file: " + outputfile);
+				gen.write(new File(outputfile));
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
